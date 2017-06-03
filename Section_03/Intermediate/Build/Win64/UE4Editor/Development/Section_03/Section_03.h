808ff7c7732379eb49506bf1a7c070827d364cab
Jose Monge <jmonge@headlesschickengames.com>
2017-06-02 20:46:17 -0600

    uploading project at current progress

--- Section_03/Intermediate/Build/Win64/UE4Editor/Development/Section_03/Section_03.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_SECTION_03_H__
-#define __AUTO_SECTION_03_H__
-//Last Write: 1/10/2017 4:34:14 AM +00:00
-#include "D:\Users\JP\Documents\Github\UnrealCourseCplusplusPart\Section_03\Source\Section_03\Section_03.h"
-#endif//__AUTO_SECTION_03_H__
